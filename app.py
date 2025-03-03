7ba5d8bacfc8432453331af765de2022746cc101
jane7166 <122887267+jane7166@users.noreply.github.com>
2025-03-04 01:46:53 +0900

    flask final modify

--- app.py
+++ app.py
@@ -339,4 +339,4 @@ def process_blogger():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
